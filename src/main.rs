0bd972488809dfcf44b60611eee82de3c90c5c51
Aria Stewart <aredridel@dinhe.net>
2020-07-03 21:34:45 -0400

    Tighten things up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use mailparse::{parse_mail, MailParseError};
+use mailparse::{parse_mail, MailHeaderMap, MailParseError};
 use std::io::{self, Read};
 
 #[derive(Debug, Eq, PartialEq)]
@@ -20,41 +20,37 @@ fn main() {
 
 fn handle(mail: &[u8]) -> Result<(), MailParseError> {
     let action = action_for_mail(mail)?;
+
     println!("{:?}", action);
+
+    match action {
+        MailingListAction::Subscribe(_email) => (),
+        MailingListAction::Unsubscribe(_email) => (),
+        MailingListAction::Reject => (),
+        MailingListAction::Message(_message) => (),
+    }
+
     return Ok(());
 }
 
 fn action_for_mail(mail: &[u8]) -> Result<MailingListAction, MailParseError> {
     let mail_rec = parse_mail(mail)?;
-    let from = mail_rec
-        .headers
-        .iter()
-        .find(|&h| &h.get_key().to_lowercase() == "from");
+    let from = mail_rec.headers.get_first_value("from");
 
     if let None = from {
         return Ok(MailingListAction::Reject);
     }
 
-    let subject = mail_rec
-        .headers
-        .iter()
-        .find(|&h| &h.get_key().to_lowercase() == "subject");
-    // If command
-    //      Handle command
-    // Else if list allows outside posting or sender is member of list
-    //      Send to list
-    // Else
-    //      reject
+    let subject = mail_rec.headers.get_first_value("subject");
 
     if let Some(subject) = subject {
-        if subject.get_value().to_lowercase().trim() == "subscribe" {
-            return Ok(MailingListAction::Subscribe(from.unwrap().get_value()));
-        } else if subject.get_value().to_lowercase().trim() == "unsubscribe" {
-            return Ok(MailingListAction::Unsubscribe(from.unwrap().get_value()));
+        if subject.to_lowercase().trim() == "subscribe" {
+            return Ok(MailingListAction::Subscribe(from.unwrap()));
+        } else if subject.to_lowercase().trim() == "unsubscribe" {
+            return Ok(MailingListAction::Unsubscribe(from.unwrap()));
         }
     }
 
-    println!("{:?}", mail_rec);
     return Ok(MailingListAction::Message(mail));
 }
 
@@ -66,7 +62,7 @@ mod test {
     #[test]
     fn a_basic_parse() {
         assert_eq!(
-            action_for_mail("Subject: test\r\n\r\n".as_bytes()).unwrap(),
+            action_for_mail(b"Subject: test\r\n\r\n").unwrap(),
             MailingListAction::Reject
         );
     }
@@ -74,8 +70,7 @@ mod test {
     #[test]
     fn a_subscribe_command() {
         assert_eq!(
-            action_for_mail("From: test@example.org\r\nSubject: subsCRIbe     \r\n\r\n".as_bytes())
-                .unwrap(),
+            action_for_mail(b"From: test@example.org\r\nSubject: subsCRIbe     \r\n\r\n").unwrap(),
             MailingListAction::Subscribe("test@example.org".to_string())
         );
     }
@@ -83,28 +78,24 @@ mod test {
     #[test]
     fn an_unsubscribe_command() {
         assert_eq!(
-            action_for_mail("From: test@example.org\r\nSubject: UNSUbscribe\r\n\r\n".as_bytes())
-                .unwrap(),
+            action_for_mail(b"From: test@example.org\r\nSubject: UNSUbscribe\r\n\r\n").unwrap(),
             MailingListAction::Unsubscribe("test@example.org".to_string())
         );
     }
 
     #[test]
     fn a_message_to_pass_through() {
-        let message = "From: test@example.org\r\nSubject: A message to the list\r\n\r\n".as_bytes();
+        let message = b"From: test@example.org\r\nSubject: A message to the list\r\n\r\n";
 
         assert_eq!(
-            action_for_mail(message)
-                .unwrap(),
+            action_for_mail(message).unwrap(),
             MailingListAction::Message(message)
         );
     }
 
     #[test]
     fn a_bad_email() {
-        if let MailParseError::Generic(msg) =
-            action_for_mail("bad input\r\n".as_bytes()).unwrap_err()
-        {
+        if let MailParseError::Generic(msg) = action_for_mail(b"bad input\r\n").unwrap_err() {
             assert_eq!(msg, "Unexpected newline in header key")
         } else {
             panic!()
